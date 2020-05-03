f5f7d7c612a756987417886063ebf4099203038b
David Calhoun <david.b.calhoun@gmail.com>
2020-05-02 22:40:23 -0400

    HoverSlideshowAnimated: add support for children, change to div container instead of anchor

--- src/HoverSlideshowAnimated.js
+++ src/HoverSlideshowAnimated.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import useHoverSlideshow from "./useHoverSlideshow";
@@ -12,30 +12,25 @@ import styles from "./HoverSlideshowAnimated.css";
  * ```npm i --save react-transition-group```
  */
 export default function HoverSlideshowAnimated(props) {
-	const { images, style, className, href, width, height } = props;
+	const { images, style, className, width, height, children, ...otherProps } = props;
 	let [
 		{ currentImage, currentImageEventId },
-		{ updateHoverSlideshow, resetHoverSlideshow }
+		{ updateHoverSlideshow, resetHoverSlideshow },
 	] = useHoverSlideshow(images);
 
-	const containerProps = {
-		href,
-		style,
-		className
-	};
-
 	return (
-		<a {...containerProps}>
-			<picture
-				onMouseLeave={resetHoverSlideshow}
-				onMouseMove={updateHoverSlideshow}
-				style={{
-					width,
-					height,
-					...style
-				}}
-				className={`${ styles.container } ${ className }`}
-			>
+		<div
+			onMouseLeave={resetHoverSlideshow}
+			onMouseMove={updateHoverSlideshow}
+			style={{
+				width,
+				height,
+				...style,
+			}}
+			className={`${styles.container} ${className}`}
+			{...otherProps}
+		>
+			<picture>
 				<TransitionGroup>
 					<CSSTransition
 						timeout={300}
@@ -47,7 +42,7 @@ export default function HoverSlideshowAnimated(props) {
 							enterDone: styles["crossfade-enter-done"],
 							exit: styles["crossfade-exit"],
 							exitActive: styles["crossfade-exit-active"],
-							exitDone: styles["crossfade-exit-done"]
+							exitDone: styles["crossfade-exit-done"],
 						}}
 						key={currentImageEventId}
 					>
@@ -57,7 +52,8 @@ export default function HoverSlideshowAnimated(props) {
 					</CSSTransition>
 				</TransitionGroup>
 			</picture>
-		</a>
+			{children}
+		</div>
 	);
 }
 
@@ -73,11 +69,11 @@ HoverSlideshowAnimated.propTypes = {
 	/** Custom CSS style overrides. */
 	style: PropTypes.object,
 	/** Width of the container, e.g. "100px" */
-	width: PropTypes.string
+	width: PropTypes.string,
 };
 
 HoverSlideshowAnimated.defaultProps = {
 	className: "",
 	images: [],
-	style: {}
+	style: {},
 };
